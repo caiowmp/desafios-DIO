34f848d5cc76cc42df2dd8b6273960649b8387c4
Caio <contato.caiomp@gmail.com>
2022-03-03 11:22:23 -0300

    commitando os desafios básicos

---
+++ Desafios basicos/Desafio1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            while(x != 0)
+            {
+              int soma = 0;
+              
+              if(x % 2 != 0)
+              {
+                x++;
+              }
+              
+              for(int  i = 0; i < 10; i+=2)
+              {
+                soma += x + i;
+              }
+              Console.WriteLine(soma);
+              x = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+  }
---
+++ REAMDE.md
@@ -0,0 +1,3 @@
+## Desafios propostos em Bootcamps da Digital Innovation One
+
+Esses desafios possuem níveis diferentes: básico, intermediário e avançado.
