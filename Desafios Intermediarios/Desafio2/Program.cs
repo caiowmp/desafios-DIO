028a0357f179ab6dcbf52cc2182e22a482798735
Caio <contato.caiomp@gmail.com>
2022-03-03 13:18:25 -0300

    Commit do Desafio2 Intermediário

---
+++ Desafios Intermediarios/Desafio2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System; 
+
+class minhaClasse {
+
+    static void Main(string[] args) { 
+
+            double a, b, c;
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+        
+            double resultado;
+
+            if( (b - c < a) && (a < b + c) && ( a - c  < b) && (b < a + c) && ( a - b  < c) && (c < a + b))
+            {
+                resultado = (a+b+c);
+                Console.WriteLine("Perimetro = " + resultado);
+            }else{
+                resultado = (((a+b)*c)/2.0);
+                Console.WriteLine("Area = {0}", + resultado);
+            }
+
+    }
+
+}
