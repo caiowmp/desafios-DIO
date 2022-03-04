ea23aad10e4bd3e7f56235b6d81ce246a7590c32
Caio <contato.caiomp@gmail.com>
2022-03-03 23:06:57 -0300

    commit padrão

--- Desafios Intermediarios/Desafio2/Program.cs
+++ Desafios Intermediarios/Desafio2/Program.cs
@@ -5,19 +5,20 @@ class minhaClasse {
     static void Main(string[] args) { 
 
             double a, b, c;
-            a = Convert.ToDouble(Console.ReadLine());
-            b = Convert.ToDouble(Console.ReadLine());
-            c = Convert.ToDouble(Console.ReadLine());
+            string[] valor = Console.ReadLine().Split('.');
+            a = Convert.ToDouble(valor[0]);
+            b = Convert.ToDouble(valor[1]);
+            c = Convert.ToDouble(valor[2]);
         
             double resultado;
 
             if( (b - c < a) && (a < b + c) && ( a - c  < b) && (b < a + c) && ( a - b  < c) && (c < a + b))
             {
                 resultado = (a+b+c);
-                Console.WriteLine("Perimetro = " + resultado);
+                Console.WriteLine("Perimetro = {0}", resultado);
             }else{
                 resultado = (((a+b)*c)/2.0);
-                Console.WriteLine("Area = {0}", + resultado);
+                Console.WriteLine("Area = {0}", resultado);
             }
 
     }
