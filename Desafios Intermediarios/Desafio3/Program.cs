1c1548c991a25c7cf3a2f158868d3f4aec389914
Caio <contato.caiomp@gmail.com>
2022-03-03 23:04:30 -0300

    Commit do desafio3 resolvido

--- Desafios Intermediarios/Desafio3/Program.cs
+++ Desafios Intermediarios/Desafio3/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class MinhaClasse {
+    public static void Main (string[] args) {
+        var totalDeCasosDeTeste = int.Parse(Console.ReadLine());
+   
+        var k = 0;
+
+        while(k != totalDeCasosDeTeste)
+        {
+            string frase = Console.ReadLine();
+
+            List<string> subString = new List<string>();
+                
+            string[] aux = frase.Split(' ');
+
+            subString.AddRange(aux);
+
+            for(int i = 0; i < subString.Count - 1; i++)
+            {
+                for(int j = i+1; j < subString.Count; j++)
+                {
+                    if (subString[i] == subString[j])
+                    {
+                        //System.Console.WriteLine("item removido: {0} ; posição: {1}",subString[j],j);
+                        subString.RemoveAt(j);
+                        j--;
+                        
+                        /*for(int f = 0; f < subString.Count; f++)
+                        {
+                            Console.Write(subString[f] + " ");
+                        }
+                        System.Console.WriteLine();
+                    }else{
+                        System.Console.WriteLine("nenhum item removido");
+                        for(int f = 0; f < subString.Count; f++)
+                        {
+                            Console.Write(subString[f] + " ");
+                        }
+                        System.Console.WriteLine();
+                    */}
+                }
+            }
+
+            subString.Sort();
+
+            for(int f = 0; f < subString.Count; f++)
+            {
+                Console.Write(subString[f] + " ");
+            }
+
+            System.Console.WriteLine();
+
+            subString.Clear();
+
+            k++;
+        }
+
+    }
+}
