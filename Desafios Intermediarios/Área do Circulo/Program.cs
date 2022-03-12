37b057034e1214a67b31d41df82f78149bad0aaa
Caio <contato.caiomp@gmail.com>
2022-03-12 17:13:26 -0300

    Desafio área do circulo. Mais uma vez tive dificuldade com a saída de doubles utilizando o'.' para indicar as casas decimais

---
+++ Desafios Intermediarios/Área do Circulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System; 
+using System.Globalization;
+
+class Classe {
+
+    static void Main(string[] args)
+    {
+        double area, raio;
+        double pi = 3.14159;
+
+        //para ler os valores em double, recebidos com '.' entre a casa inteira e decimal, de forma correta
+        NumberFormatInfo provider = new NumberFormatInfo();
+        provider.NumberDecimalSeparator = ".";
+
+        raio = double.Parse(Console.ReadLine(),provider);
+
+        area = pi * (Math.Pow(raio,2));
+
+        System.Console.WriteLine("A={0:F4}",area.ToString("G",CultureInfo.InvariantCulture));
+
+        Console.ReadKey();
+    }
+
+}
