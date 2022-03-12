97407bc4c0a3387bab1404b6d0d7d31d0f781e0d
Caio <contato.caiomp@gmail.com>
2022-03-11 21:24:30 -0300

    Desafio5, assim como em outro desafio, o fato da saída ter que ser com as casas decimais indicadas por ponto complicam a situação, consegui fazer com que saíssem assim mas somente para quando a casa decimal é diferente de 0

---
+++ Desafios basicos/Desafio5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C;
+
+            //para ler os valores em double, recebidos com '.' entre a casa inteira e decimal, de forma correta
+            NumberFormatInfo provider = new NumberFormatInfo();
+            provider.NumberDecimalSeparator = ".";
+
+            A = double.Parse(Console.ReadLine(),provider);
+            B = double.Parse(Console.ReadLine(),provider);
+            C = double.Parse(Console.ReadLine(),provider);
+            
+            double media;
+            media = ((A*2)+(B*3)+(C*5))/10;
+            
+            Console.WriteLine($"MEDIA = {media}", media.ToString("G",CultureInfo.InvariantCulture));
+            //Console.WriteLine("MEDIA = {0:F2}", media);
+            Console.ReadKey();
+        }
+    }
+}
