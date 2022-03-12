4f1aa792423ee12b295be8477c5ff73d762ce8c3
Caio <contato.caiomp@gmail.com>
2022-03-11 21:02:58 -0300

    Resolução de mais um desafio básico

---
+++ Desafios basicos/Desafio4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System; 
+ 
+class DIO{
+ 
+    static void Main(string[] args) {
+		
+        string[] input;  
+        //recebe a entrada toda em uma linha e separado por ' '
+        input = Console.ReadLine().Split(' ');
+        
+        int cod1 = int.Parse(input[0]);
+        int n1 = int.Parse(input[1]);
+        double valor1 = double.Parse(input[2]);
+    
+ 	    input = Console.ReadLine().Split(' ');
+        
+        int cod2 = int.Parse(input[0]);
+	    int n2  = int.Parse(input[1]);
+        double valor2 = double.Parse(input[2]);
+
+        double total = (n1 * valor1) + (n2 * valor2);
+
+        //imprime com duas casas decimais
+        Console.WriteLine("VALOR A PAGAR: R$ {0:F2}", total);
+    }
+
+}
