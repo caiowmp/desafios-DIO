277d02e517d2a3fa357bbf55960d0fc0f4fc3a84
Caio <contato.caiomp@gmail.com>
2022-03-11 21:45:31 -0300

    Desafio da fórmula de bhaskara

---
+++ Desafios Intermediarios/Formula de Bhaskara/Program.cs
@@ -0,0 +1,30 @@
+﻿using System; 
+
+class minhaClasse {
+
+    static void Main(string[] args) { 
+
+            double a, b, c, delta, r1, r2;
+            string[] valor = Console.ReadLine().Split();
+
+            a = Convert.ToDouble(valor[0]);
+            b = Convert.ToDouble(valor[1]);
+            c = Convert.ToDouble(valor[2]);
+
+            delta = (Math.Pow(b, 2) - (4 * a * c));
+            r1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            r2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+            if(a == 0 || delta < 0)
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+            else
+            {
+                Console.WriteLine("R1 = {0:F5}",r1);
+                Console.WriteLine("R2 = {0:F5}",r2);
+            }
+
+    }
+
+}
