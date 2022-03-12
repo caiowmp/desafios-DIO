ccd6c80db92186cc4d8e6b15b6c36f3fe20da512
Caio <contato.caiomp@gmail.com>
2022-03-11 22:07:25 -0300

    Desafio Polígonos Regulares Simples

---
+++ Desafios basicos/Polígonos Regulares Simples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System; 
+
+class Problem {
+
+    static void Main(string[] args) { 
+
+        string[] v = Console.ReadLine().Split();
+            
+        int lados = int.Parse(v[0]);
+        int comprimento = int.Parse(v[1]);
+
+        int perimetro = lados * comprimento;
+
+        System.Console.WriteLine(perimetro);
+
+    }
+
+}
