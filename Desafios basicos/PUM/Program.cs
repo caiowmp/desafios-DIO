dcee419d7ae937578881ad1be345a39e008b1ec4
Caio <contato.caiomp@gmail.com>
2022-03-11 22:00:36 -0300

    Desafio PUM

---
+++ Desafios basicos/PUM/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TESTE {
+    class Program {
+        static void Main(string[] args) {
+        
+            int N = int.Parse(Console.ReadLine());
+
+            int primeiro = 1;
+            for (int i = 1; i <= N; i++) {
+
+                Console.Write(primeiro + " ");
+                primeiro++;
+                Console.Write(primeiro + " ");
+                primeiro++;
+                Console.Write(primeiro + " ");
+                primeiro++;
+                Console.WriteLine("PUM");
+                primeiro++;
+                
+            }
+        }
+    }
+}
