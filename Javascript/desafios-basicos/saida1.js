84bbf764b5643b3f2a866bf65370536061409cd3
Caio <contato.caiomp@gmail.com>
2022-05-04 11:29:11 -0300

    Questão resolvida

---
+++ Javascript/desafios-basicos/saida1.js
@@ -0,0 +1,9 @@
+//a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+
+print('---------------------------------------');
+print('|                                     |');
+print('|                                     |');
+print('|                                     |');
+print('|                                     |');
+print('|                                     |');
+print('---------------------------------------');
