6c81d35e3a0ed82308dbbf60f28ffd2aa71664a8
Caio <contato.caiomp@gmail.com>
2022-05-04 11:17:59 -0300

    Questão resolvida

---
+++ Javascript/desafios-basicos/soma.js
@@ -0,0 +1,8 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados
+//a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+
+let A = parseInt(gets());
+let B = parseInt(gets());
+
+let SOMA = A + B;               
+print("SOMA = "+ SOMA );
