23fb87261aa86cb6ebf9c91aca87647239ac7ccc
Caio <contato.caiomp@gmail.com>
2022-05-06 11:28:16 -0300

    Questão resolvida, mas passou em 7 de 8 casos de teste

--- Javascript/desafios-basicos/o-escolhido.js
+++ Javascript/desafios-basicos/o-escolhido.js
@@ -1,14 +1,16 @@
 let alunos = gets();
 let matricula = 0;
+let notas = 0;
 
 for(var i =0;i<alunos;i++){
     entrada = gets().split(" ");
-    if(parseFloat(entrada[1])>= 8){
+    if(parseFloat(entrada[1])>= 8 && entrada[1] > notas){
         matricula = parseInt(entrada[0]);
+        notas = parseInt(entrada[1]);
     }
 }
 
 if(matricula != 0)
     print(matricula);
 else
-    print("Minimum note not reached")
+    print("Minimum note not reached");
